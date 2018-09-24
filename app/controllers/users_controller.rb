a43d9784b9859c9d21a509a3ab954b8d8c98b351
fernandezmj <fernandez.michaeljohn@gmail.com>
2018-09-24 13:35:47 +0800

    adds use sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -17,16 +18,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find params[:id]
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-    @user = User.find params[:id]
   end
 
   def update
-    @user = User.find params[:id]
     if @user.update(user_params)
       flash[:success] = "User was successfully updated"
       redirect_to articles_path
@@ -36,6 +34,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find params[:id]
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
